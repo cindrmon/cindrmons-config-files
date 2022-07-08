bdecb5537804f6788b6f5afd77823e864df370aa
cindrmon <cindrmon@protonmail.com>
2022-07-08 12:52:10 +0800

    refactor(polybar): change file name to config.ini

--- stowPackages/polybar/.config/polybar/launch.sh
+++ stowPackages/polybar/.config/polybar/launch.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q polybar;
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar_main_mon.log /tmp/polybar_sub_mon_top.log /tmp/sub_mon_bot.log
-polybar main_monitor 2>&1 | tee -a /tmp/polybar_main_mon.log & disown
-polybar sub_monitor_top 2>&1 | tee -a /tmp/polybar_sub_mon_top.log & disown
-polybar sub_monitor_bottom 2>&1 | tee -a /tmp/polybar_sub_mon_bot.log & disown
+echo "---" | tee -a /var/log/polybar/polybar_main_mon.log /var/log/polybar/polybar_sub_mon_top.log /var/log/sub_mon_bot.log
+polybar main_monitor 2>&1 | tee -a /var/log/polybar/polybar_main_mon.log & disown
+polybar sub_monitor_top 2>&1 | tee -a /var/log/polybar/polybar_sub_mon_top.log & disown
+polybar sub_monitor_bottom 2>&1 | tee -a /var/log/polybar/polybar_sub_mon_bot.log & disown
 
 echo "Bars launched..."
